3281bf9154d1b89b9da261de32e6cd6a3d42a813
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2023-02-23 11:25:40 -0300

    Criação tela de forgot password

--- src/screens/Auth/Cadastro/Cadastro.js
+++ src/screens/Auth/Cadastro/Cadastro.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { AntDesign, MaterialCommunityIcons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { View, Text, TextInput, Pressable } from 'react-native';
 
 import { styles } from './styles';
 import { HeaderLogo } from '../../../components/HeaderLogo';
 
 export const Cadastro = () => {
+    const navigation = useNavigation();
+
+    const [showPassword, setShowPassword] = useState(false);
+    const [showPasswordConfirm, setShowPasswordConfirm] = useState(false);
+
+    const handleShowPassword = () => {
+        setShowPassword(!showPassword);
+    };
+
+    const handleShowPasswordConfirm = () => {
+        setShowPasswordConfirm(!showPasswordConfirm);
+    };
+
+    const handleGoToLogin = () => {
+        navigation.navigate('Login');
+    }
+
     return (
         <View style={styles.container}>
             <HeaderLogo />
@@ -12,6 +31,68 @@ export const Cadastro = () => {
             <Text style={styles.subtitle}>
                 Registre-se para continuar
             </Text>
+
+            <View style={styles.contentInputName}>
+                <AntDesign name="user" size={24} color="lightgray" />
+                <TextInput
+                    style={styles.textInputName}
+                    placeholder='Digite seu nome'
+                />
+            </View>
+
+            <View style={styles.contentInputName}>
+                <AntDesign name="mail" size={24} color="lightgray" />
+                <TextInput
+                    style={styles.textInputName}
+                    placeholder='Digite seu e-mail'
+                />
+            </View>
+
+            <View style={styles.contentInputName}>
+                <AntDesign name="lock" size={24} color="lightgray" />
+                <TextInput
+                    style={styles.textInputName}
+                    placeholder='Digite sua senha'
+                    secureTextEntry={!showPassword}
+                    type={showPassword ? 'text' : 'password'}
+                />
+                <Pressable onPress={handleShowPassword}>
+                    <MaterialCommunityIcons
+                        name={showPassword ? "eye" : "eye-off"}
+                        size={24}
+                        color="lightgray"
+                    />
+                </Pressable>
+            </View>
+
+            <View style={styles.contentInputName}>
+                <AntDesign name="lock" size={24} color="lightgray" />
+                <TextInput
+                    style={styles.textInputName}
+                    placeholder='Repita sua senha'
+                    secureTextEntry={!showPasswordConfirm}
+                    type={showPasswordConfirm ? 'text' : 'password'}
+                />
+                <Pressable onPress={handleShowPasswordConfirm}>
+                    <MaterialCommunityIcons
+                        name={showPasswordConfirm ? "eye" : "eye-off"}
+                        size={24}
+                        color="lightgray"
+                    />
+                </Pressable>
+            </View>
+
+
+            <Pressable style={styles.buttonCadastro} >
+                <Text style={styles.textButton}>Cadastrar</Text>
+            </Pressable>
+
+            <Pressable style={styles.forgotRegister} onPress={handleGoToLogin}>
+                <Text style={styles.forgotRegisterText}>
+                    Já tenho conta?
+                    <Text style={styles.textLink} > Voltar ao Login</Text>
+                </Text>
+            </Pressable>
         </View>
     )
 }
--- src/screens/Auth/ForgotPassword/ForgotPassword.js
+++ src/screens/Auth/ForgotPassword/ForgotPassword.js
@@ -1,21 +1,47 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { View, Text, TextInput, Pressable } from 'react-native';
+
+import { styles } from './styles';
+import { HeaderLogo } from '../../../components/HeaderLogo';
 
 export const ForgotPassword = () => {
+    const navigation = useNavigation();
+
+    const handleGoToLogin = () => {
+        navigation.navigate('Login');
+    }
+
     return (
-        <View
-            style={{
-                flex: 1,
-                alignItems: 'center',
-                justifyContent: 'center'
-            }}
-        >
-            <Text
-                style={{
-                    fontSize: 30,
-                    fontWeight: 'bold'
-                }}
-            >Recuperar Senha</Text>
+        <View style={styles.container} >
+           <HeaderLogo />
+
+           <Text style={styles.subtitle}>
+                Recupere sua Senha
+           </Text>
+
+           <View style={styles.contentInputEmail}>
+                <AntDesign name="user" size={24} color="lightgray" />
+                <TextInput
+                    style={styles.textInputEmail}
+                    placeholder="Digite seu e-mail"
+                />
+           </View>
+
+           <Pressable
+                style={styles.button}
+                onPress={() => console.log('Recuperar Senha')}
+           >
+                <Text style={styles.buttonText}>Recuperar Senha</Text>
+           </Pressable>
+
+           <Pressable style={styles.forgotRegister} onPress={handleGoToLogin}>
+                <Text style={styles.forgotRegisterText}>
+                    Não vai recuperar?
+                    <Text style={styles.textLink} > Voltar ao Login</Text>
+                </Text>
+            </Pressable>
         </View>
     )
 }
